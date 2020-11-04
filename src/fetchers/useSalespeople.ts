802e1344ca51bebe7990bf19af51d56c12595ec5
Gustavo Almeida <almeidaws@outlook.com>
2020-11-04 10:36:07 -0300

    Remove useless @ts-ignore tags

--- src/fetchers/useSalespeople.ts
+++ src/fetchers/useSalespeople.ts
@@ -4,13 +4,9 @@ import { useRequest } from "@dr.cash/requesting";
 import getSalespeople from "../requests/getSalespeople";
 
 function useSalespeople(
-  // @ts-ignore
   pageNumber: number,
-  // @ts-ignore
   pageSize: number,
-  // @ts-ignore
   orderBy: "email" | "name" | "createdAt",
-  // @ts-ignore
   searchTerm: "string" | undefined
 ): Salespeople | Error | null {
   const [result, error] = useRequest(
